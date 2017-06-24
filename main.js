6a7fb2045bb6d489eb7856f9fd6bd02a73373ca3
Jimmie Harkins <ghana796@gmail.com>
2017-06-23 23:33:55 -0400

    stuffs

--- main.js
+++ main.js
@@ -1,54 +1,58 @@
-// Get the canvas element from our HTML above
-var canvas = document.getElementById("renderCanvas");
+var canvas = document.getElementById('canvas');
+canvas.width = document.documentElement.clientWidth;
+var context = canvas.getContext('2d');
+var componentList = [];
 
-// Load the BABYLON 3D engine
-var engine = new BABYLON.Engine(canvas, true);
+function ball(radius, color, x, y, xvel, yvel) {
+	this.type = "ball";
+	this.radius = radius;
+	this.x = x;
+	this.y = y;
+	this.xvel = xvel;
+	this.yvel = yvel;
+	this.color = color;
+	componentList.push(this);
+}
 
-// This begins the creation of a function that we will 'call' just after it's built
-var createScene = function () {
-	// Now create a basic Babylon Scene object 
-	var scene = new BABYLON.Scene(engine);
+function drawBall(ball) {
+	context.beginPath();
+	context.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI, false);
+	context.fillStyle = ball.color;
+	context.fill();
+	context.lineWidth = 1;
+	context.strokeStyle = '#000000';
+	context.stroke();
+}
 
-	// Change the scene background color to green.
-	scene.clearColor = new BABYLON.Color3(0, 1, 0);
+function tick() {
+	for(var i = 0; i < componentList.length; i++) {
+		var component = componentList[i];
+		component.x += component.xvel;
+		component.y += component.yvel;
+	}
+}
 
-	// This creates and positions a free camera
-	var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+function drawFrame() {
+	canvas.width = document.documentElement.clientWidth;
+	canvas.height = document.documentElement.clientHeight;
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	for(var i = 0; i < componentList.length; i++) {
+		var component = componentList[i];
+		switch(component.type) {
+			case "ball":
+				drawBall(component);
+				break;
+			default:
+				break;
+		}
+	}
+}
 
-	// This targets the camera to scene origin
-	camera.setTarget(BABYLON.Vector3.Zero());
+function gameLoop() {
+	tick();
+	drawFrame();
+}
 
-	// This attaches the camera to the canvas
-	camera.attachControl(canvas, false);
+var golfball = new ball(20, "#DDDDDD", 50, 50, 5, 0);
 
-	// This creates a light, aiming 0,1,0 - to the sky.
-	var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-
-	// Dim the light a small amount
-	light.intensity = .5;
-
-	// Let's try our built-in 'sphere' shape. Params: name, subdivisions, size, scene
-	var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-
-	// Move the sphere upward 1/2 its height
-	sphere.position.y = 1;
-
-	// Let's try our built-in 'ground' shape.  Params: name, width, depth, subdivisions, scene
-	var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
-
-	// Leave this function
-	return scene;
-};
-
-// Now, call the createScene function that you just finished creating
-var scene = createScene();
-
-// Register a render loop to repeatedly render the scene
-engine.runRenderLoop(function () {
-	scene.render();
-});
-  
-    // Watch for browser/canvas resize events
-window.addEventListener("resize", function () {
-	engine.resize();
-});
+var interval = setInterval(gameLoop, 20);
